b6c9d30dfe36d56421181374d1d65ad372f980f9
SHIDDHARTHA_PAL(221-35-1069) <105942359+Shiddharthapal@users.noreply.github.com>
2024-02-09 20:32:01 +0600

    Create 4.141. Linked List Cycle.js

---
+++ 4.141. Linked List Cycle.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if(!head) return false;
+    let first=head;
+    let last=head;
+    while(first){
+        last=last.next;
+        first=first.next?.next;/*"?" ata check kore "first.next" er value
+        null or undifined ki na. jodi null or undifined na hoi then next node
+        a move kore. r undifined or null hoyle r move kore na. mainly "?" ata
+        use kora hoi  check korar jonno. jar jonno kono error ashbe na*/
+        if(first===last) return true;
+    }
+    return false;
+};
+let data={
+    val:3,
+    next:{
+        val:2,
+        next:null,
+    },
+};
+data.next.next=data;
+console.log(hasCycle(data));
