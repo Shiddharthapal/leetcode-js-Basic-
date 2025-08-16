6b053b748e0b01d3e1533b17ee2710b2103de469
shid <shiddhartpal01355@gmail.com>
2025-08-16 14:00:26 +0600

    solve 58-beats(100%)

---
+++ Leetcode-Solve/58. Length of Last Word.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  let count = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === " ") {
+      if (count) break;
+    } else {
+      count++;
+    }
+  }
+  return count;
+};
+console.log(lengthOfLastWord("   fly me   to   the moon"));
