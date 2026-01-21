e8b24dba7353120716865fae7df6bb961965c52c
Shiddhartha Pal <pal35-1069@diu.edu.bd>
2026-01-21 20:06:35 +0600

    Again solved 83. Remove Duplicates from Sorted List from leetcode. After modification the beat percentage is low runtime beats 7.94% and memory beats 6.56%. here i am using travrsal approach with modification of the code

--- Leetcode-Solve/83. Remove Duplicates from Sorted List.js
+++ Leetcode-Solve/83. Remove Duplicates from Sorted List.js
@@ -10,50 +10,31 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
+    if(!head.length) return head;
     let map=new Map();
-    let result=null;
-    let temp=null;
-    let flag=0;
+    let temp={
+        val:head?.val,
+        next:null,
+    }
+    let result=temp;
+    map.set(head?.val,1);
+    head=head?.next;
     while(head){
-        if(!map.has(head.val)){
-            if(flag===0){
-                result={
-                    val:head.val,
-                    next:null,
-                }
-                temp=result;
-                flag=1;
-            }else{
-                temp.next={
-                    val:head.val,
-                    next:null,
-                }
-                temp=temp.next;
+        if(!map.has(head.val)){  
+            temp.next={
+                val:head.val,
+                next:null,
             }
-
+            temp=temp.next;
             map.set(head.val,1);
         }
         head=head.next;
     }
-    return result;
-    
+    return result; 
 };
 
 let data={
-    val:1,
-    next:{
-        val:1,
-        next:{
-            val:2,
-            next:{
-                val:3,
-                next:{
-                    val:3,
-                    next:null,
-                },
-            },
-        },
-    },
+    
 };
 
 console.log(deleteDuplicates(data));
