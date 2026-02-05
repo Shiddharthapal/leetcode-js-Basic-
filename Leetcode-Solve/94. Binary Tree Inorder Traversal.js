6d2d52e809280ba88fe0f1ecb4fc4c5f4593c90e
Shiddhartha Pal <pal35-1069@diu.edu.bd>
2026-02-05 13:44:04 +0600

    Solved 94. Binary Tree Inorder Traversal from leetcode. That beats runtime 100.00% and beats memory 54.86%. Here i am using tree traversal with inorder traversal (Left->mid(root)->right)

---
+++ Leetcode-Solve/94. Binary Tree Inorder Traversal.js
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    if(!root) return [];
+
+    let result=[];
+    var inorderTraversal=function(root){
+        if(!root) return;
+        inorderTraversal(root.left);
+        result.push(root.val);
+        inorderTraversal(root.right);
+    }
+    inorderTraversal(root);
+    return result;
+    
+};
+
+let data={
+    val:1,
+    left:{
+        val:2,
+        left:{
+            val:4,
+            left:null,
+            right:null,
+        },
+        right:{
+            val:5,
+            left:{
+                val:6,
+                left:null,
+                right:null,
+            },
+            right:{
+                val:7,
+                left:null,
+                right:null,
+            },
+        },
+    },
+    right:{
+        val:3,
+        left:null,
+        right:{
+            val:8,
+            left:{
+                val:9,
+                left:null,
+                right:null,
+            },
+            right:null,
+        },
+    },
+
+}
+
+console.log(inorderTraversal(data));
