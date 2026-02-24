845df3fb8d81e9f0e3f296f03a0ff208ed433571
Shiddhartha Pal <pal35-1069@diu.edu.bd>
2026-02-24 22:50:18 +0600

    Solved 123. Best Time to Buy and Sell Stock III from leetcode. That beats runtime 86.53% and beats memory 88.92%. Here i am using normal approach , there are 4 variable i use (buy1) this first varible calculate the best first buy. and 2nd (sell1) this calculate the money after selling , third (buy2) this calculate the best buy2 after selling the first. and it store the frist sell value to buy new stock.  fourth (sell2) varibale  that calculate value after selling the buy2. this store best selling among the  buy and sell transaction

--- Leetcode-Solve/123. Best Time to Buy and Sell Stock III.js
+++ Leetcode-Solve/123. Best Time to Buy and Sell Stock III.js
@@ -3,34 +3,18 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-  let first = 0;
-  let last = 0;
-  let profit = [];
-  let temp = [];
+  let buy1=-prices[0];
+  let sell1=0;
+  let buy2=-prices[0];
+  let sell2=0;
   for (let i = 1; i < prices.length; i++) {
-    if (prices[first] >= prices[i]) {
-      first = i;
-      if (profit.length > 0) {
-        profit.sort((a, b) => b - a);
-        temp.push(profit[0]);
-        profit = [];
-      }
-    } else {
-      let sum = prices[i] - prices[first];
-      last = i;
-      profit.push(sum);
-    }
+    buy1=Math.max(buy1, -prices[i]);
+    sell1=Math.max(sell1, buy1+prices[i]);
+    buy2=Math.max(buy2, sell1-prices[i]);
+    sell2=Math.max(sell2, buy2+prices[i]);
   }
 
-  if (first === prices.length - 1 && last == 0) return 0;
-  if (profit.length > 0) {
-    profit.sort((a, b) => b - a);
-    temp.push(profit[0]);
-    profit = [];
-  }
-  temp.sort((a, b) => b - a);
-  let sum=temp.length>1?temp[0]+temp[1]:temp[0];
-  return sum;
+  return sell2;
 };
 
 console.log(maxProfit([6,1,3,2,4,7]));
