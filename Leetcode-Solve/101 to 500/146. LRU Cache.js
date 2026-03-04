8da0c3228f4ac3f465755fbc4582a0a3bb890083
Shiddhartha Pal <pal35-1069@diu.edu.bd>
2026-03-04 23:25:02 +0600

    Solved 146. LRU Cache from leetcode. Here beats runtime 30.13% and beats memory 51.67%. Here i create LRUCach class to operate all operation put, get. to exacute this operation i need to connect, disconnect and movetofront sub operation

--- Leetcode-Solve/101 to 500/146. LRU Cache.js
+++ Leetcode-Solve/101 to 500/146. LRU Cache.js
@@ -2,43 +2,69 @@
  * @param {number} capacity
  */
 class LRUCache {
-    constructor(capacity){
-        this.capacity=this.capacity;
-        this.map=new Map();
-        this.head={};
-        this.tail={};
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.map = new Map();
+    this.head = {};
+    this.tail = {};
 
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+  }
+  removelastused() {
+    const [key, next, prev] = [this.head.next.key, this.head.next.next, this.head];
 
-        this.head.prev=this.tail;
-        this.tail.prev=this.head;
-    }
-    get = function(key){
-        const hasKey=map.has(key);
-        if(!hasKey) return -1;
+    this.map.delete(key);
+    this.head.next = next;
+    this.head.next.prev = prev;
+  }
 
-        let node=this.map.get(key);
-        
+  put(key, value) {
+    const haskey = this.get(key) !== -1;
+    const isatcapacity = this.map.size === this.capacity;
 
+    if (haskey) return (this.tail.prev.value = value);
+    if (isatcapacity) this.removelastused();
 
-    }
+    const node = { key, value };
+    this.map.set(key, node);
+    this.movetofront(node);
+  }
+  movetofront(node) {
+    const [prev, next] = [this.tail.prev, this.tail];
 
-    put = function(key, value) {
-    
-    };
-    
-};
+    this.tail.prev.next = node;
+    this.connectednode(node, { prev, next });
+    this.tail.prev = node;
+  }
 
+  connectednode(node, top) {
+    node.prev = top.prev;
+    node.next = top.next;
+  }
+  get(key) {
+    const hasKey = this.map.has(key);
+    if (!hasKey) return -1;
 
+    let node = this.map.get(key);
+    this.discountNode(node);
+    this.movetofront(node);
 
+    return node.value;
+  }
+  discountNode(node) {
+    node.next.prev = node.prev;
+    node.prev.next = node.next;
+  }
+}
 
-let  lRUCache = new LRUCache(2);
+let lRUCache = new LRUCache(2);
 lRUCache.put(1, 1); // cache is {1=1}
 lRUCache.put(2, 2); // cache is {1=1, 2=2}
-lRUCache.get(1);    // return 1
+lRUCache.get(1); // return 1
 lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-lRUCache.get(2);    // returns -1 (not found)
+lRUCache.get(2); // returns -1 (not found)
 lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-lRUCache.get(1);    // return -1 (not found)
-lRUCache.get(3);    // return 3
-lRUCache.get(4); 
-
+lRUCache.get(1); // return -1 (not found)
+lRUCache.get(3); // return 3
+lRUCache.get(4);
