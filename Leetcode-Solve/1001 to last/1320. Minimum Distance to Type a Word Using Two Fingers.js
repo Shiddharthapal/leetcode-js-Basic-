7e827835ffc5ba9278505104ed33f2ec98fde61e
Shiddhartha Pal <shiddhartpal01355@gmail.com>
2026-04-13 02:34:10 +0600

    Solved 1320. Minimum Distance to Type a Word Using Two Fingers  from leetcode. that beats runtime79.31% and beats memory 68.97%. Here i implement minimumDistance function to calculate typing distance using two fingers

---
+++ Leetcode-Solve/1001 to last/1320. Minimum Distance to Type a Word Using Two Fingers.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} text
+ * @return {number}
+ */
+var minimumDistance = function(text) {
+    const dist = (a, b) => {
+        if (a === 26 || b === 26) 
+            return 0;
+        return Math.abs(Math.floor(a / 6) - Math.floor(b / 6)) + Math.abs(a % 6 - b % 6);
+    };
+
+    let dp = Array(27).fill(Infinity);
+    dp[26] = 0;
+    let prev = text.charCodeAt(0) - 65;
+
+    for (let i = 1; i < text.length; i++) {
+        let cur = text.charCodeAt(i) - 65;
+        let next_dp = Array(27).fill(Infinity);
+        for (let free = 0; free < 27; free++) {
+            if (dp[free] === Infinity) 
+                continue;
+            next_dp[free] = Math.min(next_dp[free], dp[free] + dist(prev, cur));
+            next_dp[prev] = Math.min(next_dp[prev], dp[free] + dist(free, cur));
+        }
+        dp = next_dp;
+        prev = cur;
+    }
+
+    return Math.min(...dp);
+};
+
+console.log(minimumDistance("CAKE"))
